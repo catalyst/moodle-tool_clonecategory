4625d59acece6fa44dfa6c4f665565382a552296
david adamson <davidadamson@catalyst-au.net>
2023-05-10 14:44:47 +1000

    fix version

--- version.php
+++ version.php
@@ -10,7 +10,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_clonecategory';
-$plugin->version = 20121111100;
+$plugin->version = 2021111100;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_RC;
 $plugin->release = 'v1.0.rc1';
